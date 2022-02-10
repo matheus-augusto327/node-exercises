b1d16e19bf61ef4ffeddeae573e04cbdbdca463f
Matheus <matheus.augustoreal@gmail.com>
2022-02-10 18:24:55 -0300

    Learn about files in node

--- calc.js
+++ calc.js
@@ -1,11 +1,16 @@
-let calc = {
-    mult: (x, y) => {
-        return x * y
-    },
+// let calc = {
+//     mult: (x, y) => {
+//         return x * y
+//     },
 
-    soma: (x, y) => {
-        return x + y
-    }
-}
+//     soma: (x, y) => {
+//         return x + y
+//     }
+// }
 
-module.exports = calc
+// module.exports = calc
+
+// module.exports.soma = function soma(x, y) {return x + y}
+
+// module.exports.mult = function mult(x, y) {return x * y}
+
--- script.js
+++ script.js
@@ -1,16 +1,76 @@
-let calc = require("./calc")
+let fs = require("fs")
+
+// let calc = require("./calc")
+// let args = process.argv.slice(2)
+
+// let a = Number(args[1])
+// let b = Number(args[2])
+// let c = ""
+
+// if(args[0] == "s") {
+//     c = calc.soma(a, b)
+// } else if(args[0] == "m") {
+//     c = calc.mult(a, b)
+// } else {
+//     c = "Opção inválida"
+// }
+
+// console.log(c)
+
+
+
+// fs.writeFile("teste.txt", "Hello World", function(error) {
+
+//     if(error) {throw error}
+
+//     console.log("Arquivo criado com sucesso!")
+
+// })
+
+// fs.appendFile("teste.txt", " - Lorem", function(error) {
+
+//     if(error) {throw error}
+
+//     console.log("Arquivo atualizado com sucesso!")
+
+// })
+
+// fs.unlink("teste.txt", function(error) {
+
+//     if(error) {throw error}
+
+//     console.log("Arquivo apagadoS com sucesso!")
+
+// })
+
+// fs.rename("teste.txt", "Novo.txt",function(error) {
+
+//     if(error) {throw error}
+
+//     console.log("Arquivo apagadoS com sucesso!")
+
+// })
+
+// fs.readFile("Novo.txt", "UTF8",function(error, data) {
+
+//     if(error) {throw error}
+
+//     console.log(data)
+
+// })
+
 let args = process.argv.slice(2)
 
-let a = Number(args[1])
-let b = Number(args[2])
-let c = ""
+let fileName = args[0]
 
-if(args[0] == "s") {
-    c = calc.soma(a, b)
-} else if(args[0] == "m") {
-    c = calc.mult(a, b)
-} else {
-    c = "Opção inválida"
-}
+fs.readFile(fileName, "UTF8", (error, data) => {
 
-console.log(c)
+    if(error) throw error
+
+    fs.writeFile(fileName + "_uppercase", data.toUpperCase(), (error) => {
+        if (error) throw error
+    })
+
+    console.log("Arquivo gerado com sucesso!")
+
+})
