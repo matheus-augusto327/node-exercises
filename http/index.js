aa4e4122103ce9c27e91a459d016e50687554007
Matheus <matheus.augustoreal@gmail.com>
2022-02-15 20:45:41 -0300

    Learn more about http

--- http/index.js
+++ http/index.js
@@ -1,9 +1,26 @@
 const http = require("http")
+const url = require("url")
+const fs = require("fs")
 
 http.createServer((request, response) => {
 
-    response.writeHead(200, {"Content-Type": "text/html"})
-    response.end("<h1>Hello World!</h1>")
+    let path = url.parse(request.url).pathname
+    let fileName = "." + path
+
+    if(path == "" || path == "/") {
+        path = "/index.html"
+    }
+
+    fs.readFile(fileName, (err, data) => {
+        if(err) {
+            response.writeHead(404, {"Content-Type": "text/html;charset=utf-8"})
+            response.end("<h1>Página não encontrada</h1>")
+        } else {
+            response.writeHead(200, {"Content_Type": "text-html"})
+            response.write(data)
+            response.end()
+        }
+    })
 
 }).listen(3000, (err) => {
     if(err) {
